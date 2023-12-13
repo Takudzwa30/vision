3823855f7cb015496a34e0de076c5d72ea93c376
Takudzwa Allen Mushai <takudzwa.mushai@vaimanagement.co>
2023-12-13 09:18:49 +0400

    added infoCards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import InfoCards from "@/app/temporary/infoCards/InfoCards";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <InfoCards />
     </main>
   );
 }
