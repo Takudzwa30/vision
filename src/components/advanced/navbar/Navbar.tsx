f2bd12363f5511817d328d64e7efc74fe47a187c
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-12-17 15:20:16 +0400

    fixed spacing

--- src/components/advanced/navbar/Navbar.tsx
+++ src/components/advanced/navbar/Navbar.tsx
@@ -1,7 +1,11 @@
+// Styles
+import Style from "./Navbar.module.css";
+
 const Navbar: React.FC = () => {
+  // TODO : create a wrapper component with common inline padding for all components
   return (
-    <div>
-      <h1>bavbar</h1>
+    <div className={Style.navBarWrapper}>
+      <div className={Style.title}>Dashboard</div>
     </div>
   );
 };
