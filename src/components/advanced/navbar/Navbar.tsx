b4ed0fa3a9b0b34f2849787d6dd0657881c96372
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-12-18 06:18:31 +0400

    init navbar styling

--- src/components/advanced/navbar/Navbar.tsx
+++ src/components/advanced/navbar/Navbar.tsx
@@ -1,11 +1,32 @@
 // Styles
 import Style from "./Navbar.module.css";
 
-const Navbar: React.FC = () => {
+interface SidebarProps {
+  setSidebarIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  sidebarIsOpen: boolean;
+}
+
+const Navbar: React.FC<SidebarProps> = ({
+  sidebarIsOpen,
+  setSidebarIsOpen,
+}) => {
   // TODO : create a wrapper component with common inline padding for all components
   return (
     <div className={Style.navBarWrapper}>
       <div className={Style.title}>Dashboard</div>
+      <div className={Style.nav}>
+        <div
+          className={sidebarIsOpen ? Style.hamburgerActive : Style.hamburger}
+          onClick={() => {
+            setSidebarIsOpen((old: boolean) => !old);
+            document.documentElement.scrollTop = 0;
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
     </div>
   );
 };
