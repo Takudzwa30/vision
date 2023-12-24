f02ca72daaeda95e44c6bf5d3abe8206f1f9bdc3
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-12-24 19:32:28 +0400

    added login page

--- src/app/(auth)/signin/SigninView.tsx
+++ src/app/(auth)/signin/SigninView.tsx
@@ -1,9 +1,91 @@
+"use client";
+
+import React, { useState } from "react";
+
+// Libraries
+import { Formik, Field, Form, FormikHelpers } from "formik";
+
+// Styles
+import Style from "./SigninView.module.css";
+import ToggleSwitch from "@/components/ui/toggleSwitch/ToggleSwitch";
+import Link from "next/link";
+
+// Types
+interface Values {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
 const SigninView: React.FC = () => {
-    return (
-      <div>
-        <h1>SigninView</h1>
-      </div>
-    );
+  const [isChecked, setChecked] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
   };
-  export default SigninView;
-  
+  return (
+    <div className={Style.signin}>
+      <div className={Style.titles}>
+        <h3>Nice to see you!</h3>
+        <h6>Enter your email and password to sign in</h6>
+      </div>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={(
+          values: Values,
+          { setSubmitting }: FormikHelpers<Values>
+        ) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        <Form>
+          <div className={Style.fieldWrapper}>
+            <label htmlFor="email">Email</label>
+            <Field
+              id="email"
+              name="email"
+              placeholder="Your email address"
+              type="email"
+            />
+          </div>
+          <div className={Style.fieldWrapper}>
+            <label htmlFor="password">Password</label>
+            <Field
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Your password"
+            />
+          </div>
+
+          <div className={Style.switchWrapper}>
+            <ToggleSwitch
+              checked={isChecked}
+              onChange={handleChange}
+              label="Enable Feature"
+            />
+            <p>Remember me </p>
+          </div>
+
+          <button className={Style.submitBtn} type="submit">
+            Submit
+          </button>
+        </Form>
+      </Formik>
+      <div className={Style.signupWrapper}>
+        Don't have an account?{" "}
+        <span>
+          <Link href="/signup">Sign up</Link>
+        </span>
+      </div>
+    </div>
+  );
+};
+export default SigninView;
