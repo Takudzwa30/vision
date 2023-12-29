8fe17b1d0f1f9eda754eda3d209798292f99fbf2
Takudzwa Allen Mushai <takudzwa.mushai@vaimanagement.co>
2023-12-29 10:17:48 +0400

    fix

--- src/app/(main)/home/HomeView.tsx
+++ src/app/(main)/home/HomeView.tsx
@@ -2,10 +2,10 @@
 import InfoCards from "./components/infoCards/InfoCards";
 import UserDetails from "./components/userDetails/UserDetails";
 import Satisfaction from "./components/satisfaction/Satisfaction";
+import Referral from "./components/referral/Referral";
 
 // Styles
 import Style from "./HomeView.module.css";
-import Referral from "./components/referral/Referral";
 
 const HomeView: React.FC = () => {
   return (
