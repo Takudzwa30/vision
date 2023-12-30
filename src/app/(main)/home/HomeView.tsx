7a7a7f17ec50481ad3e5749105276f9d6748cda2
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2023-12-31 00:57:24 +0400

    init orders

--- src/app/(main)/home/HomeView.tsx
+++ src/app/(main)/home/HomeView.tsx
@@ -3,11 +3,12 @@ import InfoCards from "./components/infoCards/InfoCards";
 import UserDetails from "./components/userDetails/UserDetails";
 import Satisfaction from "./components/satisfaction/Satisfaction";
 import Referral from "./components/referral/Referral";
+import Sales from "./components/sales/Sales";
+import Users from "./components/users/Users";
+import Orders from "./components/orders/Orders";
 
 // Styles
 import Style from "./HomeView.module.css";
-import Sales from "./components/sales/Sales";
-import Users from "./components/users/Users";
 
 const HomeView: React.FC = () => {
   return (
@@ -23,6 +24,9 @@ const HomeView: React.FC = () => {
       <section className={Style.salesAndUsers}>
         <Sales />
         <Users />
+      </section>
+      <section className={Style.projectsAndOrders}>
+        <Orders />
       </section>
     </main>
   );
---
+++ src/app/(main)/home/components/orders/Orders.tsx
@@ -0,0 +1,12 @@
+// Styles
+import Style from "./Orders.module.css";
+
+const Orders: React.FC = () => {
+  return (
+    <div>
+      <h1>Orders</h1>
+    </div>
+  );
+};
+
+export default Orders;
