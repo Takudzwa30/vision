26e42b51b3fb361432dcf8bda9a552a377745d66
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-01-01 16:54:12 +0400

    init BillingView

---
+++ src/app/(main)/billing/BillingView.tsx
@@ -0,0 +1,15 @@
+// Components
+import CardDetails from "./components/cardDetails/CardDetails";
+
+// Styles
+import Style from "./BillingView.module.css";
+
+const BillingView: React.FC = () => {
+  return (
+    <main>
+      <CardDetails />
+    </main>
+  );
+};
+
+export default BillingView;
