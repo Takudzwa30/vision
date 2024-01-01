ceedd44d77360d38b4cde5731227c007f6d0ed9c
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-01-01 17:52:25 +0400

    init Billing

--- src/app/(main)/billing/BillingView.tsx
+++ src/app/(main)/billing/BillingView.tsx
@@ -1,6 +1,8 @@
 // Components
 import CardDetails from "./components/cardDetails/CardDetails";
 import Invoices from "./components/invoices/Invoices";
+import Transactions from "./components/transactions/Transactions";
+import BillingInfo from "./components/billingInfo/BillingInfo";
 
 // Styles
 import Style from "./BillingView.module.css";
@@ -11,6 +13,10 @@ const BillingView: React.FC = () => {
       <section className={Style.cardAndInvoices}>
         <CardDetails />
         <Invoices />
+      </section>
+      <section className={Style.billingAndTransactions}>
+        <BillingInfo />
+        <Transactions />
       </section>
     </main>
   );
---
+++ src/app/(main)/billing/components/billingInfo/BillingInfo.tsx
@@ -0,0 +1,15 @@
+// Components
+import CardWrapper from "@/components/ui/cardWrapper/CardWrapper";
+
+// Styles
+import Style from "./BillingInfo.module.css";
+
+const BillingInfo: React.FC = () => {
+  return (
+    <CardWrapper>
+      <div>BIlling</div>
+    </CardWrapper>
+  );
+};
+
+export default BillingInfo;
