2be0f6c8669a01c1cdc47f3cf6b2ea41f35e7638
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-01-01 17:52:32 +0400

    init Ttransactions

---
+++ src/app/(main)/billing/components/transactions/Transactions.tsx
@@ -0,0 +1,15 @@
+// Components
+import CardWrapper from "@/components/ui/cardWrapper/CardWrapper";
+
+// Styles
+import Style from "./Transactions.module.css";
+
+const Transactions: React.FC = () => {
+  return (
+    <CardWrapper>
+      <div>Transactions</div>
+    </CardWrapper>
+  );
+};
+
+export default Transactions;
