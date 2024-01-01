14460024d711547ad9f55b2e367e8dce98f98256
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-01-01 18:21:31 +0400

    incomplete transaction

--- src/app/(main)/billing/components/transactions/Transactions.tsx
+++ src/app/(main)/billing/components/transactions/Transactions.tsx
@@ -1,5 +1,11 @@
+// Libraries
+import Image from "next/image";
+
 // Components
 import CardWrapper from "@/components/ui/cardWrapper/CardWrapper";
+
+// Icons
+import dateIcon from "@/assets/icons/dateIcon.svg";
 
 // Styles
 import Style from "./Transactions.module.css";
@@ -7,9 +13,24 @@ import Style from "./Transactions.module.css";
 const Transactions: React.FC = () => {
   return (
     <CardWrapper>
-      <div>Transactions</div>
+      <div className={Style.heading}>
+        <h5>Your Transactions</h5>
+        <div className={Style.date}>
+          <Image alt="date Icon" src={dateIcon} />
+          <p>23 - 30 March 2020</p>
+        </div>
+      </div>
+      <div className={Style.transactionsGrid}>
+        <Transaction />
+      </div>
     </CardWrapper>
   );
+};
+
+const Transaction: React.FC = () => {
+  return <div>
+    
+  </div>;
 };
 
 export default Transactions;
