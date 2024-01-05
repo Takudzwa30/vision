341ce9e9a3af5c9e5e16ef607ff550b19f9250f5
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-01-05 22:04:05 +0400

    fix gitignore

--- .next/server/server-reference-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8re41EJ7CLivJnAy1K+ySQl5jg0Q28nwNx8VJSvW5P8=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XAxbv+Py1AxgFEogxBtoHsWDEdskVzjTOz6n/2E5NBs=\"\n}"
->>>>>>> 705c01557df266b12ac1c677b61e0c36de38cf40
