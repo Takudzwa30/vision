597f87547381acefd4db0afd5565a680dbaef22a
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-01-06 16:03:14 +0400

    init PlatformSettings

---
+++ src/app/(main)/profile/components/platformSettings/PlatformSettings.tsx
@@ -0,0 +1,12 @@
+// Styles
+import Style from "./PlatformSettings.module.css";
+
+const PlatformSettings: React.FC = () => {
+  return (
+    <div>
+      <h1>PlatformSettings</h1>
+    </div>
+  );
+};
+
+export default PlatformSettings;
