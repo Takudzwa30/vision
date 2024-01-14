a2c64107b0c3396c7c74eb547a97998e1dc96231
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-01-14 21:55:06 +0400

    fixed metadata

--- src/app/(main)/billing/page.tsx
+++ src/app/(main)/billing/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
 import BillingView from "./BillingView";
+
+export const metadata: Metadata = {
+  title: "Billing",
+};
 
 export default BillingView;
--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
 import ProfileView from "./ProfileView";
+
+export const metadata: Metadata = {
+  title: "Profile",
+};
 
 export default ProfileView;
